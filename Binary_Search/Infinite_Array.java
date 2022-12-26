d13c4dd1f3197fc05a5d9b3b0ba8ae679c3a6013
abdiqani <abdiqanimohamud143@gmail.com>
2022-12-26 16:47:04 +0000

    Binary Search

---
+++ Binary_Search/Infinite_Array.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa.Binary_Search;
+
+/**
+ *
+ * @author hp
+ */
+public class Infinite_Array {
+     public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(ans(arr, target));
+    }
+    static int ans(int[] arr, int target) {
+        // first find the range
+        // first start with a box of size 2
+        int start = 0;
+        int end = 1;
+
+        // condition for the target to lie in the range
+        while (target > arr[end]) {
+            int temp = end + 1; // this is my new start
+            // double the box value
+            // end = previous end + sizeofbox*2
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+
+    }
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+    
+}
---
+++ Binary_Search/Order_ognostic_BS.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa.Binary_Search;
+
+/**
+ *
+ * @author hp
+ */
+public class Order_ognostic_BS {
+     public static void main(String[] args) {
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+        int target = 22;
+        int ans = orderAgnosticBS(arr, target);
+        System.out.println(ans);
+    }
+
+    static int orderAgnosticBS(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+    
+}
